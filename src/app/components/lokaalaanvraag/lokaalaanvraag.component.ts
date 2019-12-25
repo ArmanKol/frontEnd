892d3b9fed060d9cbbd3fa9304556786a15d0247
ArmanKol <arman.koldaguc@gmai.com>
2019-12-25 15:41:54 +0100

    lokaalaanvraag v2.0

--- src/app/components/lokaalaanvraag/lokaalaanvraag.component.ts
+++ src/app/components/lokaalaanvraag/lokaalaanvraag.component.ts
@@ -14,6 +14,7 @@ import * as moment from 'moment'
 export class LokaalaanvraagComponent implements OnInit {
 
   lokaalaanvragenlijst = [];
+  arrByID = [];
   showSuccessMessage: boolean;
   showNotSuccessMessage: boolean;
   showLokaalMessage: boolean;
@@ -33,92 +34,188 @@ export class LokaalaanvraagComponent implements OnInit {
     await this.googleapi.getCalendarItems();
 
     if(this.volledigeCheck() && this.lokaalaanvraag.form.valid && this.lokaalaanvraag.form.get('$key').value == null){
-      //this.lokaalaanvraag.insertLokaalAanvraag(this.lokaalaanvraag.form.value, this.firebaseAuth.auth.currentUser);
+      this.lokaalaanvraag.insertLokaalAanvraag(this.lokaalaanvraag.form.value, this.firebaseAuth.auth.currentUser);
       this.showSuccessMessage = true;
       setTimeout(() => this.showSuccessMessage = false, 3000);
     }else{
-      //alert("Op dit moment is het lokaal in gebruik van: "+moment(item.start.dateTime).format('LT')+" tot "+moment(item.end.dateTime).format('LT'));
+      this.showNotSuccessMessage = true;
+      setTimeout(() => this.showNotSuccessMessage = false, 3000);
     }
   }
 
+  /*check_eindTijd_kleiner_dan_beginTijd_agenda_punt(){
+    var beginTijdOpgegeven = (<HTMLInputElement>document.getElementById("beginTijd")).value;
+    var duurOpgegeven = (<HTMLInputElement>document.getElementById("duur")).value;
+    var datumOpgegeven = (<HTMLInputElement>document.getElementById("datum")).value;
+    var eindTijd = this.getEindTijd(beginTijdOpgegeven, duurOpgegeven);
 
-  checkBegintijd_GroterDan_AgendaEindtijd(){
-    for(let afspraak of this.getAfsprakenOpDag((<HTMLInputElement>document.getElementById("datum")).value)){
-      if((<HTMLInputElement>document.getElementById("begintijd")).value >= moment(afspraak.end.dateTime).format('LT')){
-        return true;
-      }else{
-        this.showNotSuccessMessage = true;
-        setTimeout(() => this.showSuccessMessage = false, 3000);
+    for(let afspraak of this.getAfsprakenOpDag(datumOpgegeven)){
+      if(eindTijd >= new Date('1970/01/01 '+moment(afspraak.start.dateTime).format('LT'))){
+        alert('De opgegeven tijd en duur overlapt een bestaande afspraak. Probeer de duur te verkorten of neem een begintijd dat eerder begint.1');
         return false;
+      }else{
+        return true;
       }
     }
+  }*/
+
+  check_beginTijd_groter_dan_eindTijd_agenda_punt(){
+    var beginTijdOpgegeven = (<HTMLInputElement>document.getElementById("beginTijd")).value;
+    var beginTijdOpgegeven_dateObject = new Date(this.getDateVandaag(true)+beginTijdOpgegeven);
+
+    var duurOpgegeven = (<HTMLInputElement>document.getElementById("duur")).value;
+    var eindTijdOpgegeven = this.getEindTijd(beginTijdOpgegeven, duurOpgegeven);
+
+    var datumOpgegeven = (<HTMLInputElement>document.getElementById("datum")).value;
+
+    var result = false;
+
+    for(let afspraak of this.getAfsprakenOpDag(datumOpgegeven)){
+      var afspraak_begintijd = new Date(afspraak.start.dateTime);
+      var afspraak_eindTijd = new Date(afspraak.end.dateTime);
+
+      if((afspraak_begintijd < eindTijdOpgegeven && afspraak_eindTijd > beginTijdOpgegeven_dateObject) ||
+         (eindTijdOpgegeven > afspraak_begintijd && afspraak_eindTijd > beginTijdOpgegeven_dateObject)){
+        alert('De opgegeven tijd en duur ligt midden in een bestaande afspraak. Probeer je begin tijd op een latere tijdstip.1');
+        return false;
+      }else if(eindTijdOpgegeven <= afspraak_begintijd || afspraak_eindTijd <= beginTijdOpgegeven_dateObject){
+        result = true;
+      }
+    }
+    return result;
   }
 
-  checkEindtijd_KleinerDan_AgendaBegintijd(){
-    for(let afspraak of this.getAfsprakenOpDag((<HTMLInputElement>document.getElementById("datum")).value)){
-      if((<HTMLInputElement>document.getElementById("eindtijd")).value <= moment(afspraak.start.dateTime).format('LT')){
-        return true;
-      }else{
-        this.showNotSuccessMessage = true;
-        setTimeout(() => this.showSuccessMessage = false, 3000);
+  check_beginTijd_groter_dan_eindTijd_database_afspraak_tijd(){ //Opgegeven afspraak na de afspraak dat al in de database staat.
+    var beginTijdOpgegeven = (<HTMLInputElement>document.getElementById("beginTijd")).value;
+    var beginTijdOpgegeven_dateObject = new Date(this.getDateVandaag(true)+beginTijdOpgegeven);
+    var duurOpgegeven = (<HTMLInputElement>document.getElementById("duur")).value;
+    var eindTijdOpgegeven = this.getEindTijd(beginTijdOpgegeven, duurOpgegeven);
+    var result = false;
+
+    for(let afspraak of this.lokaalaanvragenlijst){
+      var afspraak_begintijd = new Date(this.getDateVandaag(true)+afspraak.begintijd);
+      var afspraak_eindTijd = new Date(this.getDateVandaag(true)+afspraak.eindtijd);
+      if((afspraak_begintijd < eindTijdOpgegeven && afspraak_eindTijd > beginTijdOpgegeven_dateObject) ||
+         (eindTijdOpgegeven > afspraak_begintijd && afspraak_eindTijd > beginTijdOpgegeven_dateObject)){
+        alert('De opgegeven tijd en duur ligt midden in een bestaande afspraak. Probeer je begin tijd op een latere tijdstip.');
         return false;
+      }else if(eindTijdOpgegeven <= afspraak_begintijd || afspraak_eindTijd <= beginTijdOpgegeven_dateObject){
+        result = true;
       }
+    }
+    return result;
+  }
+
+  /*check_eindTijd_kleiner_dan_beginTijd_database_afspraak_tijd(){ //Opgegeven afspraak voor de afspraak dat al in de database staat.
+    var beginTijdOpgegeven = (<HTMLInputElement>document.getElementById("beginTijd")).value;
+    var beginTijdOpgegeven_date = new Date('1970/01/01 '+ (<HTMLInputElement>document.getElementById("beginTijd")).value);
+    var duurOpgegeven = (<HTMLInputElement>document.getElementById("duur")).value;
+    var eindTijd = this.getEindTijd(beginTijdOpgegeven, duurOpgegeven);
+
+    var result = false;
+
+    for(let afspraak of this.lokaalaanvragenlijst){
+      var afspraak_begintijd = new Date('1970/01/01 '+afspraak.begintijd);
+      var afspraak_eindtijd = new Date('1970/01/01 '+afspraak.eindtijd);
+      console.log(afspraak);
+      if(eindTijd > afspraak_begintijd && beginTijdOpgegeven_date < afspraak_eindtijd){
+        alert('De opgegeven tijd en duur overlapt een bestaande afspraak. Probeer de duur te verkorten of neem een begintijd dat eerder begint.');
+        return false;
+      }else if(eindTijd <= afspraak_begintijd){
+        result = true;
+      }
+    }
+    return result;
+  }*/
+
+  optionsOpbouwen(){
+    var dateVandaag = new Date();
+    var selectobject = (<HTMLInputElement>document.getElementById("beginTijd"));
+    var array = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30',
+                '13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30',
+                '18:00','18:30','19:00','19:30','20:00','20:30','21:00'];
+
+    for(let afspraak of this.lokaalaanvragenlijst){
+       if(moment(afspraak.datum).format('L') === moment(dateVandaag).format('L')){
+         array = array.filter(item => item !== afspraak.begintijd);
+       }
+    }
+
+    for(let value of array){
+      var option = document.createElement("option");
+      option.setAttribute("value", value);
+      option.text = value;
+      selectobject.appendChild(option);
     }
   }
 
-  checkBegintijd_HalfuurEerderDan_AgendaTijd(){
-    for(let afspraak of this.getAfsprakenOpDag((<HTMLInputElement>document.getElementById("datum")).value)){
-      if((<HTMLInputElement>document.getElementById("begintijd")).value <= moment(afspraak.start.dateTime).subtract(0.5, 'hours').format('LT')){
-        return true;
-      }else{
-        this.showNotSuccessMessage = true;
-        setTimeout(() => this.showSuccessMessage = false, 3000);
-        return false;
-      }
+
+
+  getEindTijd(beginTijd: string, duur: string){
+    var begin_tijd:string = beginTijd;
+    var duur_tijd:string = duur;
+
+    var stringSplit_beginTijd = begin_tijd.split(":");
+    var stringSplit_duurTijd = duur_tijd.split(":");
+
+    var date = new Date(null, null, null, Number(stringSplit_beginTijd[0]), Number(stringSplit_beginTijd[1]), 0);
+    var date2 = new Date(null, null, null, Number(stringSplit_duurTijd[0]), Number(stringSplit_duurTijd[1]), 0);
+
+    var urenPlus = date.getHours() + date2.getHours();
+    var minutenPlus = date.getMinutes() + date2.getMinutes();
+
+    if(minutenPlus == 60){
+      minutenPlus = Number('00');
+      urenPlus += 1;
     }
+    return new Date(this.getDateVandaag(true)+(urenPlus + ":" + minutenPlus));
   }
 
-  checkBegintijd_NietHetzelfde(){
-    for(let afspraak of this.getAfsprakenOpDag((<HTMLInputElement>document.getElementById("datum")).value)){
-      if(!(moment(afspraak.start.dateTime).format('LT') === (<HTMLInputElement>document.getElementById("begintijd")).value)){
-        return true;
-      }else{
-        this.showNotSuccessMessage = true;
-        setTimeout(() => this.showSuccessMessage = false, 3000);
-        return false;
-      }
-    }
-  }
+  getDateVandaag(metSpatie:boolean){
+    var vandaagDate = new Date();
 
-  checkTijd(){
-    if(this.checkBegintijd_HalfuurEerderDan_AgendaTijd() && this.checkBegintijd_NietHetzelfde() && this.checkEindtijd_KleinerDan_AgendaBegintijd()
-      && this.checkBegintijd_GroterDan_AgendaEindtijd()){
-        return true;
+    var dag = vandaagDate.getDate();
+    var maand = vandaagDate.getMonth()+1;
+    var jaar = vandaagDate.getFullYear();
+
+    if(metSpatie){
+      var returnString = jaar+'/'+maand+'/'+dag+' ';
     }else{
-      return false;
+      var returnString = jaar+'/'+maand+'/'+dag;
     }
+
+    return returnString;
   }
 
   getAfsprakenOpDag(datum: string){
-    moment.lang('nl');
+    moment.locale('nl');
     var items = new Array();
+
     for(let item of this.googleapi.calendarItems){
-      if(moment(item.start.dateTime).format('YYYY-MM-DD') === datum){
+      if(moment(item.start.dateTime).format('YYYY-MM-DD') == datum){
         items.push(item);
       }else{
-        return null;
+        //return null;
       }
     }
     return items;
   }
 
   volledigeCheck(){
-    if((this.getAfsprakenOpDag((<HTMLInputElement>document.getElementById("datum")).value) == null)){
-      return true;
-    }else if(this.checkTijd() && (this.getAfsprakenOpDag((<HTMLInputElement>document.getElementById("datum")).value) != null)){
-      return true;
+    var googleLijstAfspraken = this.getAfsprakenOpDag((<HTMLInputElement>document.getElementById("datum")).value);
+    if(googleLijstAfspraken.length > 0){
+      if(this.check_beginTijd_groter_dan_eindTijd_agenda_punt() /*&& this.check_eindTijd_kleiner_dan_beginTijd_agenda_punt()*/){
+        this.getDateVandaag(true);
+        return true;
+      }else{
+        return false;
+      }
     }else{
-      return false;
+      if(this.check_beginTijd_groter_dan_eindTijd_database_afspraak_tijd() /*&& this.check_eindTijd_kleiner_dan_beginTijd_database_afspraak_tijd()*/){
+        return true;
+      }else{
+        return false;
+      }
     }
   }
 
@@ -131,7 +228,25 @@ export class LokaalaanvraagComponent implements OnInit {
     this.router.navigateByUrl('/');
   }
 
+  filterByID(obj) {
+    if (obj.payload.val().status == "progress") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   ngOnInit() {
+    this.lokaalaanvraag.getLokaalAanvragen().subscribe( list =>{
+      this.arrByID = list.filter(this.filterByID)
+      this.lokaalaanvragenlijst = this.arrByID.map(item =>{
+          return {
+            key: item.key,
+            ...item.payload.val()
+          }
+      })
+      this.optionsOpbouwen();
+    });
   }
 
 }
